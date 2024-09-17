c08a0c1ebf662abffc071de97fd2ff7a37900738
Jose Juarez Junior <josejuarez.junior@ziliatech.com>
2024-09-17 17:38:55 -0300

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,19 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<StoreContext>(options => 
+{
+	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline - The Middlewares:
+app.MapControllers();
+
+app.Run();
